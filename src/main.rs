7e8bfefc77252e43f34553eb7fa96d183cfcaa00
Sudeep Biswas <w3bdev@protonmail.com>
2024-02-08 23:27:34 +0530

    disable short help, accept height argument

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::path::PathBuf;
-use clap::Parser;
-use image::GenericImageView;
+use clap::{ArgAction, Parser};
+use image::{DynamicImage, GenericImageView};
 
 #[derive(Parser, Debug)]
+#[command(disable_help_flag = true)]
 struct Args {
     #[arg(
         short, long,
@@ -20,17 +21,35 @@ struct Args {
 
     #[arg(
         short, long,
+        value_name = "HEIGHT",
+        requires("width"),
+    )]
+    height: Option<u32>,
+
+    #[arg(
+        short, long,
         value_name = "FILE",
     )]
     output: Option<PathBuf>,
+
+    #[arg(long, action = ArgAction::HelpLong)]
+    help: Option<u8>,
 }
 
 fn main() {
     let args = Args::parse();
     if let Ok(img) = image::open(args.image) {
         if let Some(width) = args.width {
-            let new_height = width * img.height() / img.width();
-            let new_img = img.resize(width, new_height, image::imageops::FilterType::CatmullRom);
+            let new_img: DynamicImage;
+            match args.height {
+                Some(height) => {
+                    new_img = img.resize_exact(width, height, image::imageops::FilterType::CatmullRom);
+                }
+                None => {
+                    let height = width * img.height() / img.width();
+                    new_img = img.resize(width, height, image::imageops::FilterType::CatmullRom);
+                }
+            };
             let output_path = args.output.unwrap();
             match new_img.save(&output_path) {
                 Ok(_) => println!("File saved to: {:?}", output_path),
