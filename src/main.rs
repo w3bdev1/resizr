ea82efc97857e1d835e8d8d6d76f271c24edc211
Sudeep Biswas <w3bdev@protonmail.com>
2024-02-08 23:52:09 +0530

    extract resize and save function

--- src/main.rs
+++ src/main.rs
@@ -40,26 +40,31 @@ fn main() {
     let args = Args::parse();
     if let Ok(img) = image::open(args.image) {
         if let Some(width) = args.width {
-            let new_img: DynamicImage;
-            match args.height {
-                Some(height) => {
-                    new_img = img.resize_exact(width, height, image::imageops::FilterType::CatmullRom);
-                }
-                None => {
-                    let height = width * img.height() / img.width();
-                    new_img = img.resize(width, height, image::imageops::FilterType::CatmullRom);
-                }
-            };
+            let new_img = resize(&img, width, args.height);
             let output_path = args.output.unwrap();
-            match new_img.save(&output_path) {
-                Ok(_) => println!("File saved to: {:?}", output_path),
-                Err(msg) => eprintln!("Error while saving at {:?}: {}", output_path, msg),
-            }
+            save(&new_img, &output_path)
         } else {
             let (width, height) = img.dimensions();
             println!("Width: {width}, Height: {height}");
         }
     } else {
         eprintln!("Image not found");
+    }
+}
+
+fn resize(img: &DynamicImage, width: u32, height: Option<u32>) -> DynamicImage {
+    match height {
+        Some(height) => return img.resize_exact(width, height, image::imageops::FilterType::CatmullRom),
+        None => {
+            let height = width * img.height() / img.width();
+            return img.resize(width, height, image::imageops::FilterType::CatmullRom);
+        }
+    };
+}
+
+fn save(img: &DynamicImage, output_path: &PathBuf) {
+    match img.save(output_path) {
+        Ok(_) => println!("File saved to: {:?}", output_path),
+        Err(msg) => eprintln!("Error while saving at {:?}: {}", output_path, msg),
     }
 }
